6325690f7e333349c37ef0cff55912be6322b39d
Erik Lindbom <elindbom@kth.se>
2017-11-28 15:56:17 +0100

    Fixed the script issue

--- MemoryAudio/Assets/Scripts/playSong.cs
+++ MemoryAudio/Assets/Scripts/playSong.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+
 
 public class playSong : MonoBehaviour {
 
+	public Button c;
+	public Button g;
+	public Button a;
+	public Button f;
+	public Button e;
+	public Button d;
+
+	public List<int> tones;
+
+
+	void Awake(){
+		print ("Skalman");
+	}
+
 	// Use this for initialization
 	void Start () {
-		
+
+		Debug.Log("hejjd");	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void PlaySong(){
+		for(int i = 0; i < tones.Count; i++){
+			print(tones[i]);
+
+		}
 	}
+
 }
